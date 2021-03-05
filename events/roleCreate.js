117458d291e7d17b545b7cfb37f42e4fb4b5047d
Fatih EGE <54741040+fatihege@users.noreply.github.com>
2021-03-05 14:01:55 +0300

    Update roleCreate.js

--- events/roleCreate.js
+++ events/roleCreate.js
@@ -5,20 +5,26 @@ module.exports = {
     once: false,
     async run(role) {
         if (role.guild) {
-					if (await db.fetch("logch_" + role.guild.id)) {
-						const logChannel = await db.fetch("logch_" + role.guild.id);
-						const logch = role.guild.channels.cache.find(ch => ch.id === logChannel);
-				
-						let logEmbed = {
-								color: 0xe60ffa,
-								title: `Rol Oluşturuldu`,
-								description: `**Ad:** ${role.name}\n**Renk:** ${role.hexColor}\n**Bahsedilebilir:** ${role.mentionable ? "Evet" : "Hayır"}\n**Bulunduğu Sıra:** ${role.position}`
-						};
-						
-						return logch.send({ embed: logEmbed });
-					}
-				} else {
-					console.log("hata");
-				}
+            if (await db.fetch("logch_" + role.guild.id)) {
+                const logChannel = await db.fetch("logch_" + role.guild.id);
+                const logch = role.guild.channels.cache.find(
+                    (ch) => ch.id === logChannel
+                );
+
+                let logEmbed = {
+                    color: 0xe60ffa,
+                    title: `Rol Oluşturuldu`,
+                    description: `**Ad:** ${role.name}\n**Renk:** ${
+                        role.hexColor
+                    }\n**Bahsedilebilir:** ${
+                        role.mentionable ? "Evet" : "Hayır"
+                    }\n**Bulunduğu Sıra:** ${role.position}`
+                };
+
+                return logch.send({ embed: logEmbed });
+            }
+        } else {
+            console.log("hata");
+        }
     }
-}
+};
