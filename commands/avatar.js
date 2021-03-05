0e5a1584829d49ca568143c824f33d772d92ccb4
Fatih EGE <54741040+fatihege@users.noreply.github.com>
2021-03-05 13:44:43 +0300

    Update avatar.js

--- commands/avatar.js
+++ commands/avatar.js
@@ -9,39 +9,63 @@ module.exports = {
     run(message, args, client) {
         if (!message.mentions.users.size && args.length) {
             return message.reply("Böyle bir üye yok!");
-        } else if(!message.mentions.users.size) {
+        } else if (!message.mentions.users.size) {
             const commandEmbed = {
                 color: 0xe60ffa,
                 title: "Kullanıcı Avatarı",
-                description: message.author.displayAvatarURL({format: "png", dynamic: true, size: 1024}),
+                description: message.author.displayAvatarURL({
+                    format: "png",
+                    dynamic: true,
+                    size: 1024
+                }),
                 image: {
-                    url: message.author.displayAvatarURL({format: "png", dynamic: true, size: 1024})
+                    url: message.author.displayAvatarURL({
+                        format: "png",
+                        dynamic: true,
+                        size: 1024
+                    })
                 }
-            }
+            };
             return message.channel.send({ embed: commandEmbed });
         }
-        const avatarList = message.mentions.users.map(user => {
+        const avatarList = message.mentions.users.map((user) => {
             if (message.author.username == user.username) {
                 const commandEmbed = {
                     color: 0xe60ffa,
                     title: "Kullanıcı Avatarı",
-                    description: message.author.displayAvatarURL({format: "png", dynamic: true, size: 1024}),
+                    description: message.author.displayAvatarURL({
+                        format: "png",
+                        dynamic: true,
+                        size: 1024
+                    }),
                     image: {
-                        url: message.author.displayAvatarURL({format: "png", dynamic: true, size: 1024})
+                        url: message.author.displayAvatarURL({
+                            format: "png",
+                            dynamic: true,
+                            size: 1024
+                        })
                     }
-                }
+                };
                 return message.channel.send({ embed: commandEmbed });
             } else {
                 const commandEmbed = {
                     color: 0xe60ffa,
                     title: user.username + " Kullanıcısının Avatarı",
-                    description: user.displayAvatarURL({format: "png", dynamic: true, size: 1024}),
+                    description: user.displayAvatarURL({
+                        format: "png",
+                        dynamic: true,
+                        size: 1024
+                    }),
                     image: {
-                        url: user.displayAvatarURL({format: "png", dynamic: true, size: 1024})
+                        url: user.displayAvatarURL({
+                            format: "png",
+                            dynamic: true,
+                            size: 1024
+                        })
                     }
-                }
+                };
                 return message.channel.send({ embed: commandEmbed });
             }
         });
     }
-}
+};
