dbb04ca41fbede2817e8a988e02b964956fcdc9a
Fatih EGE <54741040+fatihege@users.noreply.github.com>
2021-03-05 13:55:14 +0300

    Update channelUpdate.js

--- events/channelUpdate.js
+++ events/channelUpdate.js
@@ -6,20 +6,24 @@ module.exports = {
     async run(oldChannel, newChannel, client) {
         if (oldChannel.guild) {
             if (await db.fetch("logch_" + oldChannel.guild.id)) {
-                const logChannel = await db.fetch("logch_" + oldChannel.guild.id);
-                const logch = oldChannel.guild.channels.cache.find(ch => ch.id === logChannel);
-            
+                const logChannel = await db.fetch(
+                    "logch_" + oldChannel.guild.id
+                );
+                const logch = oldChannel.guild.channels.cache.find(
+                    (ch) => ch.id === logChannel
+                );
+
                 let logEmbed = {
                     color: 0xe60ffa,
                     title: "Kanal Güncellendi",
-										description: ""
+                    description: ""
                 };
-								
-								if (oldChannel.name.toString() != newChannel.name.toString()) {
-									logEmbed.description = `<#${newChannel.id}> kanalı güncellendi.\n\n**Eski Ad:** ${oldChannel.name}\n**Yeni Ad:** ${newChannel.name}`;
-									return logch.send({ embed: logEmbed });
-								}
+
+                if (oldChannel.name.toString() != newChannel.name.toString()) {
+                    logEmbed.description = `<#${newChannel.id}> kanalı güncellendi.\n\n**Eski Ad:** ${oldChannel.name}\n**Yeni Ad:** ${newChannel.name}`;
+                    return logch.send({ embed: logEmbed });
+                }
             }
         }
     }
-}
+};
