d41787059d4967d394598a0d58a45e1a5a31038a
Fatih EGE <54741040+fatihege@users.noreply.github.com>
2021-03-05 13:50:04 +0300

    Update mdel.js

--- commands/mdel.js
+++ commands/mdel.js
@@ -9,21 +9,27 @@ module.exports = {
     run(message, args, client) {
         const amount = parseInt(args[0]);
         if (isNaN(amount)) {
-            return message.reply(`Girilen değer bir sayı olmalı! ${amount} bir sayı değil.`)
-                .then(msg => {
+            return message
+                .reply(
+                    `Girilen değer bir sayı olmalı! ${amount} bir sayı değil.`
+                )
+                .then((msg) => {
                     msg.delete({ timeout: 5000 });
-                }).catch(err => {
+                })
+                .catch((err) => {
                     console.error(err);
                 });
         } else if (amount < 2 || amount > 100) {
-            return message.reply("Silinecek mesaj sayısı 2 ile 100 arasında olmalıdır!")
-                .then(msg => {
+            return message
+                .reply("Silinecek mesaj sayısı 2 ile 100 arasında olmalıdır!")
+                .then((msg) => {
                     msg.delete({ timeout: 5000 });
-                }).catch(err => {
+                })
+                .catch((err) => {
                     console.error(err);
                 });
         }
-				message.delete();
+        message.delete();
         message.channel.bulkDelete(amount);
     }
-}
+};
