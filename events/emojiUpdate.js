e38c0c5e13c60dadb0b74bf18d861ca761c6158d
Fatih EGE <54741040+fatihege@users.noreply.github.com>
2021-03-05 13:56:13 +0300

    Update emojiUpdate.js

--- events/emojiUpdate.js
+++ events/emojiUpdate.js
@@ -7,26 +7,32 @@ module.exports = {
         if (oldEmoji.guild) {
             if (await db.fetch("logch_" + oldEmoji.guild.id)) {
                 const logChannel = await db.fetch("logch_" + oldEmoji.guild.id);
-                const logch = oldEmoji.guild.channels.cache.find(ch => ch.id === logChannel);
-            
+                const logch = oldEmoji.guild.channels.cache.find(
+                    (ch) => ch.id === logChannel
+                );
+
                 let logEmbed = {
                     color: 0xe60ffa,
                     title: "Emoji Güncellendi",
-										description: oldEmoji.id,
-										fields: [
-											{
-												name: "|-**Önce**-|",
-												value: `**Adı:** ${oldEmoji.name}\n**Animasyon:** ${oldEmoji.animated ? "Var" : "Yok"}\n**Resim:** ${oldEmoji.url}`
-											},
-											{
-												name: "|-**Sonra**-|",
-												value: `**Adı:** ${newEmoji.name}\n**Animasyon:** ${newEmoji.animated ? "Var" : "Yok"}\n**Resim:** ${newEmoji.url}`
-											},
-										]
+                    description: oldEmoji.id,
+                    fields: [
+                        {
+                            name: "|-**Önce**-|",
+                            value: `**Adı:** ${oldEmoji.name}\n**Animasyon:** ${
+                                oldEmoji.animated ? "Var" : "Yok"
+                            }\n**Resim:** ${oldEmoji.url}`
+                        },
+                        {
+                            name: "|-**Sonra**-|",
+                            value: `**Adı:** ${newEmoji.name}\n**Animasyon:** ${
+                                newEmoji.animated ? "Var" : "Yok"
+                            }\n**Resim:** ${newEmoji.url}`
+                        }
+                    ]
                 };
-								
-								return logch.send({ embed: logEmbed });
+
+                return logch.send({ embed: logEmbed });
             }
         }
     }
-}
+};
