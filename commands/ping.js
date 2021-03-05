22d3ae9d72e7d3ba500a79e2cf5812ebe675e340
Fatih EGE <54741040+fatihege@users.noreply.github.com>
2021-03-05 13:51:00 +0300

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -1,17 +1,17 @@
 module.exports = {
-	name: "ping",
-	description: "Botun ping değerini öğrenin.",
-	aliases: ["botping"],
-	args: false,
-	usage: null,
-	guildOnly: false,
-	permissions: null,
-	run(message, args, client) {
-		const pingEmbed = {
+    name: "ping",
+    description: "Botun ping değerini öğrenin.",
+    aliases: ["botping"],
+    args: false,
+    usage: null,
+    guildOnly: false,
+    permissions: null,
+    run(message, args, client) {
+        const pingEmbed = {
             color: 0xe60ffa,
-			description: `Ping değerim: **${client.ws.ping}ms**`
-		}
+            description: `Ping değerim: **${client.ws.ping}ms**`
+        };
 
-		message.channel.send({ embed: pingEmbed });
-	}
-}
+        message.channel.send({ embed: pingEmbed });
+    }
+};
