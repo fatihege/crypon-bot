a4fc7a56f22d620e7dd730645cab15d5388a544b
Fatih EGE <54741040+fatihege@users.noreply.github.com>
2021-03-05 13:52:51 +0300

    Update setprefix.js

--- commands/setprefix.js
+++ commands/setprefix.js
@@ -1,23 +1,26 @@
 const db = require("wio.db");
 
 module.exports = {
-	name: "sprefix",
-	description: "Botun önekini değiştirin.",
-	aliases: ["setprefix", "setpref", "prefixayarla"],
-	args: true,
-	usage: "<prefix>",
-	guildOnly: true,
-	permissions: "MANAGE_MESSAGES",
-	async run(message, args, client) {
+    name: "sprefix",
+    description: "Botun önekini değiştirin.",
+    aliases: ["setprefix", "setpref", "prefixayarla"],
+    args: true,
+    usage: "<prefix>",
+    guildOnly: true,
+    permissions: "MANAGE_MESSAGES",
+    async run(message, args, client) {
         try {
             await db.set("prefix_" + message.guild.id, args[0]);
-            message.channel.send(`Prefix başarıyla \`${args[0]}\` olarak ayarlandı.`)
-                .then(msg => {
+            message.channel
+                .send(`Prefix başarıyla \`${args[0]}\` olarak ayarlandı.`)
+                .then((msg) => {
                     msg.delete({ timeout: 5000 });
                 });
         } catch (error) {
             console.error(error);
-            message.channel.send("Prefix değiştirilirken bir hatayla karşılaşıyorum.");
+            message.channel.send(
+                "Prefix değiştirilirken bir hatayla karşılaşıyorum."
+            );
         }
-	}
-}
+    }
+};
