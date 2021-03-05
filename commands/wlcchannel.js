8dc8c8374e8fa8484036c0a45d251472cdeedb30
Fatih EGE <54741040+fatihege@users.noreply.github.com>
2021-03-05 13:54:09 +0300

    Update wlcchannel.js

--- commands/wlcchannel.js
+++ commands/wlcchannel.js
@@ -3,23 +3,31 @@ const db = require("wio.db");
 module.exports = {
     name: "wlcchannel",
     description: "Sunucuya katılan üyelerin karşılanacağı kanalı belirtin.",
-    aliases: ["wlcch", "welcomech", "welcomechannel", "hgkanali", "hosgeldinizkanali"],
+    aliases: [
+        "wlcch",
+        "welcomech",
+        "welcomechannel",
+        "hgkanali",
+        "hosgeldinizkanali"
+    ],
     args: true,
     usage: "<channel>",
     guildOnly: true,
     permissions: "MANAGE_CHANNELS",
     async run(message, args, client) {
         if (!message.mentions.channels.first()) {
-            message.reply("Lütfen üyeleri karşılayacağım seç!")
-                .then(msg => {
-                    msg.delete({ timeout: 5000 });
-                });
+            message.reply("Lütfen üyeleri karşılayacağım seç!").then((msg) => {
+                msg.delete({ timeout: 5000 });
+            });
         }
 
         if (message.mentions.channels.first().type != "text") {
-            return message.reply("Üyeleri karşılayacağım kanal metin kanalı olmak zorunda!")
-                .then(msg => {
-                    msg.delete({ timeout: 5000});
+            return message
+                .reply(
+                    "Üyeleri karşılayacağım kanal metin kanalı olmak zorunda!"
+                )
+                .then((msg) => {
+                    msg.delete({ timeout: 5000 });
                 });
         }
 
@@ -28,16 +36,21 @@ module.exports = {
         if (message.guild) {
             try {
                 await db.set("welcomech_" + message.guild.id, welcomech.id);
-                message.channel.send(`Üyeleri karşılama kanalı başarıyla ${welcomech} olarak ayarlandı.`)
-                    .then(msg => {
+                message.channel
+                    .send(
+                        `Üyeleri karşılama kanalı başarıyla ${welcomech} olarak ayarlandı.`
+                    )
+                    .then((msg) => {
                         msg.delete({ timeout: 5000 });
                     });
             } catch (error) {
                 console.error(error);
-                message.channel.send("Üyelerin karşılanacağı kanal ayarlanırken bir hatayla karşılaşıyorum.");
+                message.channel.send(
+                    "Üyelerin karşılanacağı kanal ayarlanırken bir hatayla karşılaşıyorum."
+                );
             }
         } else {
             welcomech.send("Sunucu bilgisi alınamadı.");
         }
     }
-}
+};
