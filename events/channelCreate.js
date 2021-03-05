bcebd9051026d69da524363ec625b48960e8cb1c
Fatih EGE <54741040+fatihege@users.noreply.github.com>
2021-03-05 13:54:31 +0300

    Update channelCreate.js

--- events/channelCreate.js
+++ events/channelCreate.js
@@ -7,16 +7,18 @@ module.exports = {
         if (channel.guild) {
             if (await db.fetch("logch_" + channel.guild.id)) {
                 const logChannel = await db.fetch("logch_" + channel.guild.id);
-                const logch = channel.guild.channels.cache.find(ch => ch.id === logChannel);
-            
+                const logch = channel.guild.channels.cache.find(
+                    (ch) => ch.id === logChannel
+                );
+
                 let logEmbed = {
                     color: 0xe60ffa,
                     title: "Kanal Oluşturuldu",
-										description: `<#${channel.id}> kanalı oluşturuldu.`
-                }
-								
-								return logch.send({ embed: logEmbed });
+                    description: `<#${channel.id}> kanalı oluşturuldu.`
+                };
+
+                return logch.send({ embed: logEmbed });
             }
         }
     }
-}
+};
