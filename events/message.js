5dc03934a71481cd367f9e2d84bf66b27563d7f5
Fatih EGE <54741040+fatihege@users.noreply.github.com>
2021-03-05 13:59:53 +0300

    Update message.js

--- events/message.js
+++ events/message.js
@@ -11,13 +11,16 @@ module.exports = {
             } else {
                 var prefix = "!c";
             }
-            
+
             if (
                 message.content.toLowerCase().trim().toString() === "chelp" ||
                 message.content.toLowerCase().trim().toString() === "prefix"
             ) {
-                message.channel.send(`Bu sunucudaki prefixim \`${prefix}\`\nBütün komutlarımın listesine ulaşmak için \`${prefix}help\` komutunu kullanabilirsin.`)
-                    .then(msg => {
+                message.channel
+                    .send(
+                        `Bu sunucudaki prefixim \`${prefix}\`\nBütün komutlarımın listesine ulaşmak için \`${prefix}help\` komutunu kullanabilirsin.`
+                    )
+                    .then((msg) => {
                         msg.delete({ timeout: 5000 });
                     });
                 return;
@@ -28,10 +31,13 @@ module.exports = {
 
         const args = message.content.slice(prefix.length).trim().split(/ +/);
         const commandName = args.shift().toLowerCase();
-        const command = client.commands.get(commandName)
-            || client.commands.find(cmd => cmd.aliases && cmd.aliases.includes(commandName))
+        const command =
+            client.commands.get(commandName) ||
+            client.commands.find(
+                (cmd) => cmd.aliases && cmd.aliases.includes(commandName)
+            );
         if (!command) return;
-        
+
         if (command.guildOnly && message.channel.type == "dm") {
             return message.reply("Bu komutu özel mesajlarda kullanamazsın!");
         }
@@ -39,23 +45,27 @@ module.exports = {
         if (command.args && !args.length) {
             let reply = `Lütfen mesaja bir argüman ver ${message.author}!`;
             if (command.usage) {
-                reply += `\nBu komutun kullanımı: \`${prefix}${command.name} ${command.usage}\``
+                reply += `\nBu komutun kullanımı: \`${prefix}${command.name} ${command.usage}\``;
             }
             return message.channel.send(reply);
         }
-    
+
         if (command.permissions) {
             const authorPerms = message.channel.permissionsFor(message.author);
             if (!authorPerms || !authorPerms.has(command.permissions)) {
-                return message.channel.send(`**Bunu yapamazsın ${message.author}!** Yeterli yetkin yok.`);
+                return message.channel.send(
+                    `**Bunu yapamazsın ${message.author}!** Yeterli yetkin yok.`
+                );
             }
         }
-    
+
         try {
             command.run(message, args, client);
         } catch (error) {
             console.error(error);
-            message.channel.send(`Bu komut çalıştırılırken bir hata meydana geldi.`);
+            message.channel.send(
+                `Bu komut çalıştırılırken bir hata meydana geldi.`
+            );
         }
     }
-}
+};
