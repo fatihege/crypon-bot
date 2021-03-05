09a6117f7b7dfbea8c42d1dcdfda95f047c29a0b
Fatih EGE <54741040+fatihege@users.noreply.github.com>
2021-03-05 13:56:57 +0300

    Update guildBanRemove.js

--- events/guildBanRemove.js
+++ events/guildBanRemove.js
@@ -6,15 +6,17 @@ module.exports = {
     async run(guild, user, client) {
         if (await db.fetch("logch_" + guild.id)) {
             const logChannel = await db.fetch("logch_" + guild.id);
-            const logch = guild.channels.cache.find(ch => ch.id === logChannel);
-            
+            const logch = guild.channels.cache.find(
+                (ch) => ch.id === logChannel
+            );
+
             let logEmbed = {
                 color: 0xe60ffa,
                 title: `Kullanıcı Engeli Kaldırıldı`,
-								description: `<@${user.id}> kullanıcısının engeli kaldırıldı.`
+                description: `<@${user.id}> kullanıcısının engeli kaldırıldı.`
             };
 
-						return logch.send({ embed: logEmbed });
+            return logch.send({ embed: logEmbed });
         }
     }
-}
+};
