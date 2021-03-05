974faced953c205ae93915affb96313b9fbc4651
Fatih EGE <54741040+fatihege@users.noreply.github.com>
2021-03-05 13:51:19 +0300

    Update reload.js

--- commands/reload.js
+++ commands/reload.js
@@ -1,4 +1,4 @@
-const fs = require('fs');
+const fs = require("fs");
 
 module.exports = {
     name: "reload",
@@ -10,33 +10,45 @@ module.exports = {
     permissions: "MANAGE_ROLES",
     run(message, args, client) {
         if (!args.length) {
-            return message.channel.send(`Lütfen yeniden yüklenecek komutun adını gir ${message.author}!`);
+            return message.channel.send(
+                `Lütfen yeniden yüklenecek komutun adını gir ${message.author}!`
+            );
         }
         const commandName = args[0].toLowerCase();
-        const command = message.client.commands.find(cmd => cmd.aliases && cmd.aliases.includes(commandName))
-            || message.client.commands.get(commandName);
+        const command =
+            message.client.commands.find(
+                (cmd) => cmd.aliases && cmd.aliases.includes(commandName)
+            ) || message.client.commands.get(commandName);
 
         if (!command) {
             return message.channel.send(`Böyle bir komut tanımlı değil!`);
         }
-        
+
         const commandFiles = fs.readdirSync("./");
-        const fileName = commandFiles.find(file => fs.readdirSync(`./${file}`).includes(`${commandName}.js`));
-        
+        const fileName = commandFiles.find((file) =>
+            fs.readdirSync(`./${file}`).includes(`${commandName}.js`)
+        );
+
         delete require.cache[require.resolve(`./${command.name}.js`)];
 
         try {
             const newCommand = require(`./${command.name}.js`);
             message.client.commands.set(newCommand.name, newCommand);
-            message.channel.send(`**\`${command.name}\`** komutu başarıyla yeniden yüklendi!`)
-                .then(msg => {
+            message.channel
+                .send(
+                    `**\`${command.name}\`** komutu başarıyla yeniden yüklendi!`
+                )
+                .then((msg) => {
                     msg.delete({ timeout: 5000 });
-                }).catch(err => {
+                })
+                .catch((err) => {
                     console.error(err);
                 });
         } catch (error) {
             console.error(error);
-            message.channel.send(`**\`${command.name}\`** komutu yüklenirken bir hata meydana geliyor.\nHata: ${error.message}`);
+            message.channel.send(
+                `**\`${command.name}\`** komutu yüklenirken bir hata meydana geliyor.\nHata: ${error.message}`
+            );
         }
     }
-}
+};
