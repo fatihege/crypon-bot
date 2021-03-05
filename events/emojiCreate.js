0de789fe33cdcffd87bd0cab81afb953e2197bdd
Fatih EGE <54741040+fatihege@users.noreply.github.com>
2021-03-05 13:55:36 +0300

    Update emojiCreate.js

--- events/emojiCreate.js
+++ events/emojiCreate.js
@@ -7,16 +7,22 @@ module.exports = {
         if (emoji.guild) {
             if (await db.fetch("logch_" + emoji.guild.id)) {
                 const logChannel = await db.fetch("logch_" + emoji.guild.id);
-                const logch = emoji.guild.channels.cache.find(ch => ch.id === logChannel);
-            
+                const logch = emoji.guild.channels.cache.find(
+                    (ch) => ch.id === logChannel
+                );
+
                 let logEmbed = {
                     color: 0xe60ffa,
                     title: "Emoji Oluşturuldu",
-										description: `**Adı:** ${emoji.name}\n**ID:** ${emoji.id}\n**Animasyon:** ${emoji.animated ? "Var" : "Yok"}\n**Resim:** ${emoji.url}`
+                    description: `**Adı:** ${emoji.name}\n**ID:** ${
+                        emoji.id
+                    }\n**Animasyon:** ${
+                        emoji.animated ? "Var" : "Yok"
+                    }\n**Resim:** ${emoji.url}`
                 };
-								
-								return logch.send({ embed: logEmbed });
+
+                return logch.send({ embed: logEmbed });
             }
         }
     }
-}
+};
