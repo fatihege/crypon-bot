8cbe5ee8462e05d5fe0a036dbda13bbce944508c
Fatih EGE <54741040+fatihege@users.noreply.github.com>
2021-03-05 13:56:34 +0300

    Update guildBanAdd.js

--- events/guildBanAdd.js
+++ events/guildBanAdd.js
@@ -6,19 +6,24 @@ module.exports = {
     async run(guild, user, client) {
         if (await db.fetch("logch_" + guild.id)) {
             const logChannel = await db.fetch("logch_" + guild.id);
-            const logch = guild.channels.cache.find(ch => ch.id === logChannel);
-            
+            const logch = guild.channels.cache.find(
+                (ch) => ch.id === logChannel
+            );
+
             let logEmbed = {
                 color: 0xe60ffa,
-									author: {
-										name: `${user.username}#${user.discriminator}`,
-										icon_url: user.displayAvatarURL({ format: "png", dynamic: true })
-									},
+                author: {
+                    name: `${user.username}#${user.discriminator}`,
+                    icon_url: user.displayAvatarURL({
+                        format: "png",
+                        dynamic: true
+                    })
+                },
                 title: `Üye Engellendi`,
-								description: `<@${user.id}> sunucudan engellendi.`
+                description: `<@${user.id}> sunucudan engellendi.`
             };
 
-						return logch.send({ embed: logEmbed });
+            return logch.send({ embed: logEmbed });
         }
     }
-}
+};
