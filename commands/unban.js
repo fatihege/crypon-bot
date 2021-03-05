ac1bfc6670bf059377938e6e2639db84712d4e02
Fatih EGE <54741040+fatihege@users.noreply.github.com>
2021-03-05 13:53:46 +0300

    Update unban.js

--- commands/unban.js
+++ commands/unban.js
@@ -1,24 +1,31 @@
 module.exports = {
-	name: "unban",
-	description: "Bir kullanıcının engelini kaldırın.",
-	aliases: ["userunban", "kullaniciengelkaldir"],
-	args: true,
-	usage: "<user-id>",
-	guildOnly: true,
-	permissions: 'BAN_MEMBERS',
-	run(message, args, client) {
-		if (!args.length) {
-			return message.reply("Lütfen engelini kaldırmak istediğiniz üyenin ID'sini belirt!");
-		}
+    name: "unban",
+    description: "Bir kullanıcının engelini kaldırın.",
+    aliases: ["userunban", "kullaniciengelkaldir"],
+    args: true,
+    usage: "<user-id>",
+    guildOnly: true,
+    permissions: "BAN_MEMBERS",
+    run(message, args, client) {
+        if (!args.length) {
+            return message.reply(
+                "Lütfen engelini kaldırmak istediğiniz üyenin ID'sini belirt!"
+            );
+        }
         try {
             message.guild.members.unban(args[0]);
-            message.channel.send(`${args[0]} ID'li kullanıcının yasağı başarıyla kaldırıldı!`)
-				.then(msg => {
-					msg.delete({ timeout: 5000 });
-				});
+            message.channel
+                .send(
+                    `${args[0]} ID'li kullanıcının yasağı başarıyla kaldırıldı!`
+                )
+                .then((msg) => {
+                    msg.delete({ timeout: 5000 });
+                });
         } catch (error) {
-            message.channel.send(`Kullanıcının engeli kaldırılırken bir sorun oluştu.`);
+            message.channel.send(
+                `Kullanıcının engeli kaldırılırken bir sorun oluştu.`
+            );
             console.error(error);
         }
-	}
-}
+    }
+};
