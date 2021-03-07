8bf159c1cb2439c91d2fbf15b81ab07f8be1ce24
Fatih EGE <54741040+fatihege@users.noreply.github.com>
2021-03-07 12:41:40 +0300

    Create unwlcchannel.js

---
+++ commands/unwlcchannel.js
@@ -0,0 +1,38 @@
+const db = require("wio.db");
+
+module.exports = {
+    name: "unwlcchannel",
+    description: "Daha önceden seçilen üyeleri karşılama kanalını silin.",
+    aliases: ["unwlcch", "hosgeldinizkanalisil", "hgkanalisil"],
+    args: false,
+    usage: null,
+    guildOnly: true,
+    permissions: "MANAGE_CHANNEL",
+    async run(message, args, client) {
+        if (
+            message.guild &&
+            (await db.fetch("welcomech_" + message.guild.id))
+        ) {
+            try {
+                await db.delete("welcomech_" + message.guild.id);
+                message.channel
+                    .send("Üyeleri karşılama kanalı başarıyla silindi!")
+                    .then((msg) => {
+                        msg.delete({ timeout: 5000 });
+                    });
+            } catch (err) {
+                message.channel
+                    .send("Bir hata ile karşılaşıldı.")
+                    .then((msg) => {
+                        msg.delete({ timeout: 5000 });
+                    });
+            }
+        } else {
+            message.channel
+                .send("Üyeleri karşılama kanalı zaten daha önceden seçilmemiş.")
+                .then((msg) => {
+                    msg.delete({ timeout: 5000 });
+                });
+        }
+    }
+};
