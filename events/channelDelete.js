8657a0fb27580830af30ca2b51d51e0e1ff69e72
Fatih EGE <54741040+fatihege@users.noreply.github.com>
2021-03-07 12:25:30 +0300

    Update channelDelete.js

--- events/channelDelete.js
+++ events/channelDelete.js
@@ -4,21 +4,25 @@ module.exports = {
     name: "channelDelete",
     once: false,
     async run(channel, client) {
-        if (channel.guild) {
-            if (await db.fetch("logch_" + channel.guild.id)) {
-                const logChannel = await db.fetch("logch_" + channel.guild.id);
-                const logch = channel.guild.channels.cache.find(
-                    (ch) => ch.id === logChannel
-                );
-
-                let logEmbed = {
-                    color: 0xe60ffa,
-                    title: "Kanal Silindi",
-                    description: `**${channel.name}** kanalı silindi.`
-                };
-
-                return logch.send({ embed: logEmbed });
+        if (channel.guild && (await db.fetch("logch_" + channel.guild.id))) {
+            if ((await db.fetch("logch_" + channel.guild.id)) == channel.id) {
+                await db.delete("logch_" + channel.guild.id);
+                return;
+            } else if (await db.fetch("welcomech_" + channel.guild.id)) {
+                await db.delete("welcomech_" + channel.guild.id);
             }
+            const logChannel = await db.fetch("logch_" + channel.guild.id);
+            const logch = channel.guild.channels.cache.find(
+                (ch) => ch.id === logChannel
+            );
+
+            let logEmbed = {
+                color: 0xe60ffa,
+                title: "Kanal Silindi",
+                description: `**${channel.name}** kanalı silindi.`
+            };
+
+            return logch.send({ embed: logEmbed });
         }
     }
 };
