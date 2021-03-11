b53e1928135b0299f3fbd527e2ba84bdbf085d44
Fatih EGE <54741040+fatihege@users.noreply.github.com>
2021-03-11 18:22:57 +0300

    Update minfo.js

--- commands/minfo.js
+++ commands/minfo.js
@@ -50,61 +50,94 @@ module.exports = {
     description: "Kendinizin veya başka bir üyenin bilgilerini görün.",
     aliases: ["memberinfo", "uyebilgi", "ubilgi"],
     args: false,
-    usage: "<member[optional]>",
+    usage: "<üye[opsiyonel]>",
     guildOnly: true,
     permissions: null,
     run(message, args, client) {
-        if (!message.mentions.users.first()) {
-            const user = message.author;
-            const member = message.member;
+        let userid;
+        if (message.mentions.users.size) {
+            userid = message.mentions.users.first().id;
+        } else {
+            if (args.length) {
+                userid = args[0];
+            } else {
+                userid = message.author.id;
+            }
+        }
+
+        const user = message.guild.members.cache.find((m) => m.id == userid);
+
+        if (!user) {
+            return message.channel.send("Böyle bir üye yok!").then((msg) => {
+                msg.delete({ timeout: 5000 });
+            });
+        }
+
+        if (userid == message.author.id) {
+            const member = message.guild.members.cache.find(
+                (m) => m.id == user.id
+            ).user;
             const userRegisteredAt = dateFormat(
-                user.createdAt,
+                member.createdAt,
                 "d mmmm dddd yyyy, h.MM.ss TT"
             );
             const userJoinedAt = dateFormat(
-                member.joinedAt,
+                user.joinedAt,
                 "d mmmm dddd yyyy, h.MM.ss TT"
             );
             let commandEmbed = {
                 color: 0xe60ffa,
+                thumbnail: {
+                    url: user.user.displayAvatarURL({
+                        format: "png",
+                        dynamic: false
+                    })
+                },
                 title: message.author.username + " Kullanıcısının Bilgileri",
                 description: `**Kullanıcı adınız:** ${message.author.username}\n**ID:** ${message.author.id}\n**Hesap oluşturma:** ${userRegisteredAt}\n**Sunucuya katılma:** ${userJoinedAt}\n**Roller:** `
             };
-            member.roles.cache.map((role) => {
+
+            user.roles.cache.map((role) => {
                 if (role.name[0] == "@") {
-                    let r = message.guild.roles.cache.get(role.id).name;
-                    commandEmbed.description += r + " ";
+                    commandEmbed.description += role.name;
                 } else {
-                    let r = message.guild.roles.cache.get(role.id).name;
-                    commandEmbed.description += "@" + r + " ";
+                    commandEmbed.description += "<@&" + role.id + ">";
                 }
             });
+
             message.channel.send({ embed: commandEmbed });
         } else {
-            const user = message.mentions.users.first();
-            const member = message.mentions.members.first();
+            const member = message.guild.members.cache.find(
+                (m) => m.id == user.id
+            ).user;
             const userRegisteredAt = dateFormat(
-                user.createdAt,
+                member.createdAt,
                 "d mmmm dddd yyyy, h.MM.ss TT"
             );
             const userJoinedAt = dateFormat(
-                member.joinedAt,
+                user.joinedAt,
                 "d mmmm dddd yyyy, h.MM.ss TT"
             );
             let commandEmbed = {
                 color: 0xe60ffa,
-                title: user.username + " Kullanıcısının Bilgileri",
-                description: `**Kullanıcı adı:** ${user.username}\n**ID:** ${user.id}\n**Hesap oluşturma:** ${userRegisteredAt}\n**Sunucuya katılma:** ${userJoinedAt}\n**Roller:** `
+                thumbnail: {
+                    url: user.user.displayAvatarURL({
+                        format: "png",
+                        dynamic: false
+                    })
+                },
+                title: user.user.username + " Kullanıcısının Bilgileri",
+                description: `**Kullanıcı adı:** ${user.user.username}\n**ID:** ${user.id}\n**Hesap oluşturma:** ${userRegisteredAt}\n**Sunucuya katılma:** ${userJoinedAt}\n**Roller:** `
             };
-            member.roles.cache.map((role) => {
+
+            user.roles.cache.map((role) => {
                 if (role.name[0] == "@") {
-                    let r = message.guild.roles.cache.get(role.id).name;
-                    commandEmbed.description += r + " ";
+                    commandEmbed.description += role.name;
                 } else {
-                    let r = message.guild.roles.cache.get(role.id).name;
-                    commandEmbed.description += "@" + r + " ";
+                    commandEmbed.description += "<@&" + role.id + ">";
                 }
             });
+
             message.channel.send({ embed: commandEmbed });
         }
     }
