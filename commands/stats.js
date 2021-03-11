c116938bce27b731fadffcc606eda7aa9d9ca965
Fatih EGE <54741040+fatihege@users.noreply.github.com>
2021-03-11 14:24:30 +0300

    Update stats.js

--- commands/stats.js
+++ commands/stats.js
@@ -16,12 +16,9 @@ module.exports = {
         );
         const ping = `${client.ws.ping} ms`;
         const memoryUsageCalculation = (
-            process.memoryUsage().heapUsed /
-            1024 /
-            1024
-        )
-            .toString()
-            .split(".");
+            process.memoryUsage().heapUsed / 1024 / 1024)
+                .toString()
+                .split(".");
         const memoryUsage = `${memoryUsageCalculation[0]},${memoryUsageCalculation[1][0]}${memoryUsageCalculation[1][1]}`;
 
         const uptime = pretty(client.uptime)
