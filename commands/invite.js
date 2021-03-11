77fbb04c8a9983f6bd2f6854d4ae543c65ebc5b1
Fatih EGE <54741040+fatihege@users.noreply.github.com>
2021-03-11 18:18:45 +0300

    Update invite.js

--- commands/invite.js
+++ commands/invite.js
@@ -1,22 +1,24 @@
 module.exports = {
-	name: "invite",
-	description: "Botun davet linkini gönderir.",
-	aliases: ["inv", "davet"],
-	args: false,
-	usage: null,
-	guildOnly: false,
-	permissions: null,
-	run(message, args, client) {
+    name: "invite",
+    description: "Botun davet linkini gönderir.",
+    aliases: ["inv", "davet"],
+    args: false,
+    usage: null,
+    guildOnly: false,
+    permissions: null,
+    run(message, args, client) {
         const messageEmbed = {
             color: 0xff14b9,
             title: "Botu Davet Et",
-            description: "https://discord.com/oauth2/authorize?client_id=815184711416152094&scope=bot&permissions=8",
+            description:
+                "https://discord.com/oauth2/authorize?client_id=815184711416152094&scope=bot&permissions=8",
             footer: {
                 text: "Fatih EGE",
-                icon_url: "https://imgupload.io/images/2021/02/28/bot-avatar.png"
+                icon_url:
+                    "https://imgupload.io/images/2021/02/28/bot-avatar.png"
             }
-        }
+        };
 
         message.channel.send({ embed: messageEmbed });
-	}
-}
+    }
+};
