5e1baecd0900676bb612e32c8111c84f2adebcf6
Fatih EGE <54741040+fatihege@users.noreply.github.com>
2021-03-11 18:34:18 +0300

    Update unwlcchannel.js

--- commands/unwlcchannel.js
+++ commands/unwlcchannel.js
@@ -9,30 +9,24 @@ module.exports = {
     guildOnly: true,
     permissions: "MANAGE_CHANNEL",
     async run(message, args, client) {
-        if (
-            message.guild &&
-            (await db.fetch("welcomech_" + message.guild.id))
-        ) {
-            try {
-                await db.delete("welcomech_" + message.guild.id);
-                message.channel
-                    .send("Üyeleri karşılama kanalı başarıyla silindi!")
-                    .then((msg) => {
-                        msg.delete({ timeout: 5000 });
-                    });
-            } catch (err) {
-                message.channel
-                    .send("Bir hata ile karşılaşıldı.")
-                    .then((msg) => {
-                        msg.delete({ timeout: 5000 });
-                    });
-            }
-        } else {
-            message.channel
-                .send("Üyeleri karşılama kanalı zaten daha önceden seçilmemiş.")
-                .then((msg) => {
-                    msg.delete({ timeout: 5000 });
-                });
-        }
+			if (message.guild && await db.fetch("welcomech_" + message.guild.id)) {
+				try {
+					await db.delete("welcomech_" + message.guild.id);
+					message.channel.send("Üyeleri karşılama kanalı başarıyla silindi!")
+						.then((msg) => {
+							msg.delete({ timeout: 5000 });
+						});
+				} catch (err) {
+					message.channel.send("Bir hata ile karşılaşıldı.")
+						.then((msg) => {
+							msg.delete({ timeout: 5000 });
+						});
+				}
+			} else {
+				message.channel.send("Üyeleri karşılama kanalı zaten daha önceden seçilmemiş.")
+					.then((msg) => {
+						msg.delete({ timeout: 5000 });
+					});
+			}
     }
-};
+}
