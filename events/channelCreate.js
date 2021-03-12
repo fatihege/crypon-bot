3c4547435a2aa7805cb806751bd8478de6333608
Fatih EGE <54741040+fatihege@users.noreply.github.com>
2021-03-12 10:45:50 +0300

    Update channelCreate.js

--- events/channelCreate.js
+++ events/channelCreate.js
@@ -11,11 +11,21 @@ module.exports = {
                     (ch) => ch.id === logChannel
                 );
 
-                let logEmbed = {
-                    color: 0xe60ffa,
-                    title: "Kanal Oluşturuldu",
-                    description: `<#${channel.id}> kanalı oluşturuldu.`
-                };
+                let logEmbed;
+
+                if (channel.type == "category") {
+                    logEmbed = {
+                        color: 0xe60ffa,
+                        title: "Kategori Oluşturuldu",
+                        description: `**${channel.name}** kategorisi oluşturuldu.`
+                    };
+                } else {
+                    logEmbed = {
+                        color: 0xe60ffa,
+                        title: "Kanal Oluşturuldu",
+                        description: `<#${channel.id}> kanalı oluşturuldu.`
+                    };
+                }
 
                 return logch.send({ embed: logEmbed });
             }
