a8604c3dd435957e70c1eb7de2ac6d0171fa084d
Fatih EGE <54741040+fatihege@users.noreply.github.com>
2021-03-15 16:22:14 +0300

    Update channelCreate.js

--- events/channelCreate.js
+++ events/channelCreate.js
@@ -1,34 +1,39 @@
-const db = require("wio.db");
+const db = require('wio.db');
+const translate = require('../language/translate');
 
 module.exports = {
-    name: "channelCreate",
-    once: false,
-    async run(channel, client) {
-        if (channel.guild) {
-            if (await db.fetch("logch_" + channel.guild.id)) {
-                const logChannel = await db.fetch("logch_" + channel.guild.id);
-                const logch = channel.guild.channels.cache.find(
-                    (ch) => ch.id === logChannel
-                );
+	name: 'channelCreate',
+	once: false,
+	async run(channel, client) {
+		if (channel.guild && (await db.fetch('logch_' + channel.guild.id))) {
+			const logChannel = await db.fetch('logch_' + channel.guild.id);
+			const logch = channel.guild.channels.cache.find((ch) => ch.id === logChannel);
 
-                let logEmbed;
+			let logEmbed;
 
-                if (channel.type == "category") {
-                    logEmbed = {
-                        color: 0xe60ffa,
-                        title: "Kategori Oluşturuldu",
-                        description: `**${channel.name}** kategorisi oluşturuldu.`
-                    };
-                } else {
-                    logEmbed = {
-                        color: 0xe60ffa,
-                        title: "Kanal Oluşturuldu",
-                        description: `<#${channel.id}> kanalı oluşturuldu.`
-                    };
-                }
+			if (channel.type == 'category') {
+				logEmbed = {
+					color: 0xe60ffa,
+					title: translate(channel, 'events.channelCreate.messages.categoryCreatedTitle'),
+					description: translate(
+						channel,
+						'events.channelCreate.messages.categoryCreatedDescription',
+						channel.name
+					),
+				};
+			} else {
+				logEmbed = {
+					color: 0xe60ffa,
+					title: translate(channel, 'events.channelCreate.messages.channelCreatedTitle'),
+					description: translate(
+						channel,
+						'events.channelCreate.messages.channelCreatedDescription',
+						channel.id
+					),
+				};
+			}
 
-                return logch.send({ embed: logEmbed });
-            }
-        }
-    }
+			return logch.send({ embed: logEmbed });
+		}
+	},
 };
