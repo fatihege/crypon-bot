0f363289840a3effc8c6d85a418327debd287de0
Fatih EGE <54741040+fatihege@users.noreply.github.com>
2021-03-15 16:19:06 +0300

    Update unban.js

--- commands/unban.js
+++ commands/unban.js
@@ -1,33 +1,45 @@
+const translate = require('../language/translate');
+
 module.exports = {
-    name: "unban",
-    description: "Bir kullanıcının engelini kaldırın.",
-    aliases: ["userunban", "kullaniciengelkaldir"],
-    args: true,
-    usage: "<user-id>",
-    guildOnly: true,
-    permissions: "BAN_MEMBERS",
-    run(message, args, client) {
-        if (!args.length) {
-            return message.reply(
-                "Lütfen engelini kaldırmak istediğiniz üyenin ID'sini belirt!"
-            );
-        }
-        try {
-            message.guild.members.unban(args[0]);
-            message.channel
-                .send(
-                    `<@${args[0]}> kullanıcısının yasağı başarıyla kaldırıldı!`
-                )
-                .then((msg) => {
-                    msg.delete({ timeout: 5000 });
-                });
-        } catch (error) {
-            message.channel
-                .send(`Kullanıcının engeli kaldırılırken bir sorun oluştu.`)
-                .then((msg) => {
-                    msg.delete({ timeout: 5000 });
-                });
-            console.error(error);
-        }
-    }
+	name: 'unban',
+	description: null,
+	aliases: ['userunban', 'kullaniciengelkaldir'],
+	args: true,
+	usage: null,
+	guildOnly: true,
+	permissions: 'BAN_MEMBERS',
+	run(message, args, client) {
+		this.description = translate(message, 'commands.unban.description');
+		this.usage = translate(message, 'commands.unban.usage');
+
+		if (isNaN(parseInt(args[0])) || args[0].length < 18) {
+			return message.channel.send(translate(message, 'commands.unban.messages.memberNotExists')).then((msg) => {
+				msg.delete({ timeout: 5000 });
+			});
+		}
+
+		message.guild.fetchBan(args[0]).then((ban) => {
+			if (ban.user.id) {
+				message.guild.members.unban(args[0]);
+				return message.channel
+					.send(translate(message, 'commands.unban.messages.successful', args[0]))
+					.then((msg) => {
+						msg.delete({ timeout: 5000 });
+					});
+			} else {
+				return message.channel
+					.send(translate(message, 'commands.unban.messages.memberNotBanned'))
+					.then((msg) => {
+						msg.delete({ timeout: 5000 });
+					});
+			}
+		}).catch((err) => {
+			console.error(err);
+			return message.channel
+				.send(translate(message, 'commands.unban.messages.memberNotBanned'))
+				.then((msg) => {
+					msg.delete({ timeout: 5000 });
+				});
+		})
+	},
 };
