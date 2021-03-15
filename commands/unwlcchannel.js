40b83005f393e718b6c952443d2c99677202841b
Fatih EGE <54741040+fatihege@users.noreply.github.com>
2021-03-15 16:19:58 +0300

    Update unwlcchannel.js

--- commands/unwlcchannel.js
+++ commands/unwlcchannel.js
@@ -1,32 +1,33 @@
-const db = require("wio.db");
+const db = require('wio.db');
+const translate = require('../language/translate');
 
 module.exports = {
-    name: "unwlcchannel",
-    description: "Daha önceden seçilen üyeleri karşılama kanalını silin.",
-    aliases: ["unwlcch", "hosgeldinizkanalisil", "hgkanalisil"],
-    args: false,
-    usage: null,
-    guildOnly: true,
-    permissions: "MANAGE_CHANNEL",
-    async run(message, args, client) {
-			if (message.guild && await db.fetch("welcomech_" + message.guild.id)) {
-				try {
-					await db.delete("welcomech_" + message.guild.id);
-					message.channel.send("Üyeleri karşılama kanalı başarıyla silindi!")
-						.then((msg) => {
-							msg.delete({ timeout: 5000 });
-						});
-				} catch (err) {
-					message.channel.send("Bir hata ile karşılaşıldı.")
-						.then((msg) => {
-							msg.delete({ timeout: 5000 });
-						});
-				}
-			} else {
-				message.channel.send("Üyeleri karşılama kanalı zaten daha önceden seçilmemiş.")
-					.then((msg) => {
-						msg.delete({ timeout: 5000 });
-					});
+	name: 'unwlcchannel',
+	description: null,
+	aliases: ['unwlcch', 'hosgeldinizkanalisil', 'hgkanalisil'],
+	args: false,
+	usage: null,
+	guildOnly: true,
+	permissions: 'MANAGE_CHANNEL',
+	async run(message, args, client) {
+		this.description = translate(message, 'commands.unwlcchannel.description');
+		this.usage = translate(message, 'commands.unwlcchannel.usage');
+
+		if (message.guild && (await db.fetch('welcomech_' + message.guild.id))) {
+			try {
+				await db.delete('welcomech_' + message.guild.id);
+				message.channel.send(translate(message, 'commands.unwlcchannel.messages.successful')).then((msg) => {
+					msg.delete({ timeout: 5000 });
+				});
+			} catch (err) {
+				message.channel.send(translate(message, 'commands.unwlcchannel.messages.errorOccurred')).then((msg) => {
+					msg.delete({ timeout: 5000 });
+				});
 			}
-    }
-}
+		} else {
+			message.channel.send(translate(message, "commands.unwlcchannel.messages.channelNotSelected")).then((msg) => {
+				msg.delete({ timeout: 5000 });
+			});
+		}
+	},
+};
