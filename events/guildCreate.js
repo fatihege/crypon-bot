109cd2b3c5d8c38415bda133a791d5a7c148cf95
Fatih EGE <54741040+fatihege@users.noreply.github.com>
2021-03-15 19:00:07 +0300

    Update guildCreate.js

--- events/guildCreate.js
+++ events/guildCreate.js
@@ -1,18 +1,29 @@
-const { botName } = require("../config.json");
-const db = require("wio.db");
+const { botName, webhookID, webhookToken } = require('../config.json');
+const db = require('wio.db');
 const translate = require('../language/translate');
+const { WebhookClient, MessageEmbed } = require('discord.js');
 
 module.exports = {
-    name: "guildCreate",
-    once: false,
-    async run(guild) {
-        await db.set("prefix_" + guild.id, "!c");
-        const prefix = await db.fetch("prefix_" + guild.id);
-        const channel = guild.channels.cache.filter(c => c.type == "text").find(ch => ch.position == 0);
-        const admin = guild.members.cache.find(m => m.hasPermission("ADMINISTRATOR"));
+	name: 'guildCreate',
+	once: false,
+	async run(guild) {
+		await db.set('prefix_' + guild.id, '!c');
+		const prefix = await db.fetch('prefix_' + guild.id);
+		const channel = guild.channels.cache.filter((c) => c.type == 'text').find((ch) => ch.position == 0);
 
-        const aChannel = guild.channels.cache.first();
+		channel.send(translate(channel, 'events.guildCreate.messages.message', botName, prefix));
 
-        channel.send(translate(aChannel, "events.guildCreate.messages.message", botName, prefix));
-    }
-}
+		const webhookClient = new WebhookClient(webhookID, webhookToken);
+		const embed = {
+			color: 0x3dff2b,
+			title: 'Yeni Sunucuya Eklendi',
+			description: `**Sunucu Adı:** ${guild.name}\n**Sunucu ID:** ${guild.id}\n**Sunucu Sahibi:** ${guild.ownerID ? "<@" + guild.ownerID + ">" : "Bilinmiyor"}\n**Sunucu Sahibi ID:** ${guild.ownerID}\n**Sunucu Üye Sayısı:** ${guild.memberCount}`,
+			timestamp: new Date(),
+		};
+		webhookClient.send("", {
+			username: 'Crypon | Logger',
+			avatarURL: "https://imgupload.io/images/2021/03/15/logger.png",
+			embeds: [embed],
+		});
+	},
+};
