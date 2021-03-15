f10c0f1a21fede254bbff3aa14415b5493cc2e88
Fatih EGE <54741040+fatihege@users.noreply.github.com>
2021-03-15 16:16:40 +0300

    Update minfo.js

--- commands/minfo.js
+++ commands/minfo.js
@@ -1,144 +1,101 @@
-const dateFormat = require("dateformat");
-dateFormat.i18n = {
-    dayNames: [
-        "Pzr",
-        "Pzt",
-        "Sal",
-        "Çrş",
-        "Prş",
-        "Cum",
-        "Cmt",
-        "Pazar",
-        "Pazartesi",
-        "Salı",
-        "Çarşamba",
-        "Perşembe",
-        "Cuma",
-        "Cumartesi"
-    ],
-    monthNames: [
-        "Ock",
-        "Şub",
-        "Mar",
-        "Nis",
-        "May",
-        "Hzn",
-        "Tem",
-        "Ağs",
-        "Eyl",
-        "Ekm",
-        "Ksm",
-        "Arl",
-        "Ocak",
-        "Şubat",
-        "Mart",
-        "Nisan",
-        "Mayıs",
-        "Haziran",
-        "Temmuz",
-        "Ağustos",
-        "Eylül",
-        "Ekim",
-        "Kasım",
-        "Aralık"
-    ],
-    timeNames: ["ö", "s", "öm", "ös", "Ö", "S", "ÖÖ", "ÖS"]
-};
+const translate = require('../language/translate');
+const dateFormat = require('dateformat');
+const db = require('wio.db');
 
 module.exports = {
-    name: "minfo",
-    description: "Kendinizin veya başka bir üyenin bilgilerini görün.",
-    aliases: ["memberinfo", "uyebilgi", "ubilgi"],
-    args: false,
-    usage: "<üye[opsiyonel]>",
-    guildOnly: true,
-    permissions: null,
-    run(message, args, client) {
-        let userid;
-        if (message.mentions.users.size) {
-            userid = message.mentions.users.first().id;
-        } else {
-            if (args.length) {
-                userid = args[0];
-            } else {
-                userid = message.author.id;
-            }
-        }
+	name: 'minfo',
+	description: null,
+	aliases: ['memberinfo', 'uyebilgi', 'ubilgi'],
+	args: false,
+	usage: null,
+	guildOnly: true,
+	permissions: null,
+	async run(message, args, client) {
+		this.description = translate(message, 'commands.minfo.description');
+		this.usage = translate(message, 'commands.minfo.usage');
 
-        const user = message.guild.members.cache.find((m) => m.id == userid);
+		let language;
+		if (message.guild && (await db.fetch('lang_' + message.guild.id))) {
+			language = await db.fetch('lang_' + message.guild.id);
+		} else if (
+			message.channel &&
+			message.channel.type == 'dm' &&
+			(await db.fetch('lang_dm_' + message.channel.id))
+		) {
+			language = await db.fetch('lang_dm_' + message.channel.id);
+		} else {
+			language = 'tr';
+		}
+		const datetimeConf = require(`../languages/${language}.json`).basic.date.datetime;
+		dateFormat.i18n = {
+			dayNames: datetimeConf.dayNames,
+			monthNames: datetimeConf.monthNames,
+			timeNames: datetimeConf.timeNames,
+		};
 
-        if (!user) {
-            return message.channel.send("Böyle bir üye yok!").then((msg) => {
-                msg.delete({ timeout: 5000 });
-            });
-        }
+		let userid;
+		if (message.mentions.users.size) {
+			userid = message.mentions.users.first().id;
+		} else {
+			if (args.length) {
+				userid = args[0];
+			} else {
+				userid = message.author.id;
+			}
+		}
 
-        if (userid == message.author.id) {
-            const member = message.guild.members.cache.find(
-                (m) => m.id == user.id
-            ).user;
-            const userRegisteredAt = dateFormat(
-                member.createdAt,
-                "d mmmm dddd yyyy, h.MM.ss TT"
-            );
-            const userJoinedAt = dateFormat(
-                user.joinedAt,
-                "d mmmm dddd yyyy, h.MM.ss TT"
-            );
-            let commandEmbed = {
-                color: 0xe60ffa,
-                thumbnail: {
-                    url: user.user.displayAvatarURL({
-                        format: "png",
-                        dynamic: false
-                    })
-                },
-                title: message.author.username + " Kullanıcısının Bilgileri",
-                description: `**Kullanıcı adınız:** ${message.author.username}\n**ID:** ${message.author.id}\n**Hesap oluşturma:** ${userRegisteredAt}\n**Sunucuya katılma:** ${userJoinedAt}\n**Roller:** `
-            };
+		const user = message.guild.members.cache.find((m) => m.id == userid);
 
-            user.roles.cache.map((role) => {
-                if (role.name[0] == "@") {
-                    commandEmbed.description += role.name;
-                } else {
-                    commandEmbed.description += "<@&" + role.id + ">";
-                }
-            });
+		if (!user) {
+			return message.channel.send(translate(message, 'commands.minfo.messages.memberNotExists')).then((msg) => {
+				msg.delete({ timeout: 5000 });
+			});
+		}
 
-            message.channel.send({ embed: commandEmbed });
-        } else {
-            const member = message.guild.members.cache.find(
-                (m) => m.id == user.id
-            ).user;
-            const userRegisteredAt = dateFormat(
-                member.createdAt,
-                "d mmmm dddd yyyy, h.MM.ss TT"
-            );
-            const userJoinedAt = dateFormat(
-                user.joinedAt,
-                "d mmmm dddd yyyy, h.MM.ss TT"
-            );
-            let commandEmbed = {
-                color: 0xe60ffa,
-                thumbnail: {
-                    url: user.user.displayAvatarURL({
-                        format: "png",
-                        dynamic: false
-                    })
-                },
-                title: user.user.username + " Kullanıcısının Bilgileri",
-                description: `**Kullanıcı adı:** ${user.user.username}\n**ID:** ${user.id}\n**Hesap oluşturma:** ${userRegisteredAt}\n**Sunucuya katılma:** ${userJoinedAt}\n**Roller:** `
-            };
+		if (userid == message.author.id) {
+			const member = message.guild.members.cache.find((m) => m.id == user.id).user;
+			const userRegisteredAt = dateFormat(member.createdAt, 'd mmmm dddd yyyy, h.MM.ss TT');
+			const userJoinedAt = dateFormat(user.joinedAt, 'd mmmm dddd yyyy, h.MM.ss TT');
+			let commandEmbed = {
+				color: 0xe60ffa,
+				thumbnail: {
+					url: user.user.displayAvatarURL({ format: 'png', dynamic: false }),
+				},
+				title: translate(message, "commands.minfo.messages.embedTitle", message.author.username),
+				description: translate(message, "commands.minfo.messages.yourselfInfo", message.author.username, message.author.id, userRegisteredAt, userJoinedAt),
+			};
 
-            user.roles.cache.map((role) => {
-                if (role.name[0] == "@") {
-                    commandEmbed.description += role.name;
-                } else {
-                    commandEmbed.description += "<@&" + role.id + ">";
-                }
-            });
+			user.roles.cache.map((role) => {
+				if (role.name[0] == '@') {
+					commandEmbed.description += role.name;
+				} else {
+					commandEmbed.description += '<@&' + role.id + '>';
+				}
+			});
 
-            message.channel.send({ embed: commandEmbed });
-        }
-    }
+			message.channel.send({ embed: commandEmbed });
+		} else {
+			const member = message.guild.members.cache.find((m) => m.id == user.id).user;
+			const userRegisteredAt = dateFormat(member.createdAt, 'd mmmm dddd yyyy, h.MM.ss TT');
+			const userJoinedAt = dateFormat(user.joinedAt, 'd mmmm dddd yyyy, h.MM.ss TT');
+			let commandEmbed = {
+				color: 0xe60ffa,
+				thumbnail: {
+					url: user.user.displayAvatarURL({ format: 'png', dynamic: false }),
+				},
+				title: translate(message, "commands.minfo.messages.embedTitle", user.user.username),
+				description: translate(message, "commands.minfo.messages.memberInfo", user.user.username, user.id, userRegisteredAt, userJoinedAt),
+			};
+
+			user.roles.cache.map((role) => {
+				if (role.name[0] == '@') {
+					commandEmbed.description += role.name;
+				} else {
+					commandEmbed.description += '<@&' + role.id + '>';
+				}
+			});
+
+			message.channel.send({ embed: commandEmbed });
+		}
+	},
 };
