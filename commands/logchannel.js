dc0602b24e6f59cdf326893c9b4c4f918b3c1d01
Fatih EGE <54741040+fatihege@users.noreply.github.com>
2021-03-15 16:16:17 +0300

    Update logchannel.js

--- commands/logchannel.js
+++ commands/logchannel.js
@@ -1,38 +1,34 @@
-const db = require("wio.db");
+const db = require('wio.db');
+const translate = require('../language/translate');
 
 module.exports = {
-    name: "logchannel",
-    description: "Sunucuda olup bitenlerin kaydını tutacağınız kanalı seçin.",
-    aliases: ["logch", "setlogch", "logkanali", "gunlukkanali"],
-    args: true,
-    usage: "<kanal>",
-    guildOnly: true,
-    permissions: "MANAGE_CHANNEL",
-    async run(message, args, client) {
-        if (!message.mentions.channels.first()) {
-            message
-                .reply("Lütfen günlük kaydını tutacağım kanalı seç!")
-                .then((msg) => {
-                    msg.delete({ timeout: 5000 });
-                });
-        }
+	name: 'logchannel',
+	description: null,
+	aliases: ['logch', 'setlogch', 'logkanali', 'gunlukkanali'],
+	args: true,
+	usage: null,
+	guildOnly: true,
+	permissions: 'MANAGE_CHANNEL',
+	async run(message, args, client) {
+		this.description = translate(message, 'commands.logchannel.description');
+		this.usage = translate(message, 'commands.logchannel.usage');
 
-        const logch = message.mentions.channels.first();
+		if (!message.mentions.channels.first()) {
+			message.reply(translate(message, 'commands.logchannel.messages.selectChannel')).then((msg) => {
+				msg.delete({ timeout: 5000 });
+			});
+		}
 
-        try {
-            await db.set("logch_" + message.guild.id, logch.id);
-            message.channel
-                .send(
-                    `Günlük kaydı kanalı başarıyla ${logch} olarak ayarlandı.`
-                )
-                .then((msg) => {
-                    msg.delete({ timeout: 5000 });
-                });
-        } catch (error) {
-            console.error(error);
-            message.channel.send(
-                "Günlük kaydı kanalı ayarlanırken bir hatayla karşılaşıyorum."
-            );
-        }
-    }
+		const logch = message.mentions.channels.first();
+
+		try {
+			await db.set('logch_' + message.guild.id, logch.id);
+			message.channel.send(translate(message, 'commands.logchannel.messages.successful', logch)).then((msg) => {
+				msg.delete({ timeout: 5000 });
+			});
+		} catch (error) {
+			console.error(error);
+			message.channel.send(translate(message, 'commands.logchannel.messages.errorOccurred'));
+		}
+	},
 };
