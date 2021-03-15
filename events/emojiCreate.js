18c9d5bfa3fae295716b6480252888073399b133
Fatih EGE <54741040+fatihege@users.noreply.github.com>
2021-03-15 16:23:35 +0300

    Update emojiCreate.js

--- events/emojiCreate.js
+++ events/emojiCreate.js
@@ -1,28 +1,34 @@
-const db = require("wio.db");
+const db = require('wio.db');
+const translate = require('../language/translate');
 
 module.exports = {
-    name: "emojiCreate",
-    once: false,
-    async run(emoji, client) {
-        if (emoji.guild) {
-            if (await db.fetch("logch_" + emoji.guild.id)) {
-                const logChannel = await db.fetch("logch_" + emoji.guild.id);
-                const logch = emoji.guild.channels.cache.find(
-                    (ch) => ch.id === logChannel
-                );
+	name: 'emojiCreate',
+	once: false,
+	async run(emoji, client) {
+		if (emoji.guild) {
+			if (await db.fetch('logch_' + emoji.guild.id)) {
+				const logChannel = await db.fetch('logch_' + emoji.guild.id);
+				const logch = emoji.guild.channels.cache.find((ch) => ch.id === logChannel);
 
-                let logEmbed = {
-                    color: 0xe60ffa,
-                    title: "Emoji Oluşturuldu",
-                    description: `**Adı:** ${emoji.name}\n**ID:** ${
-                        emoji.id
-                    }\n**Animasyon:** ${
-                        emoji.animated ? "Var" : "Yok"
-                    }\n**Resim:** ${emoji.url}`
-                };
+				const animated = emoji.animated
+					? translate(emoji, 'basic.yes')
+					: translate(emoji, 'basic.no');
 
-                return logch.send({ embed: logEmbed });
-            }
-        }
-    }
+				let logEmbed = {
+					color: 0xe60ffa,
+					title: translate(emoji, 'events.emojiCreate.messages.embedTitle'),
+					description: translate(
+						emoji,
+						'events.emojiCreate.messages.embedDescription',
+						emoji.name,
+						emoji.id,
+						animated,
+						emoji.url
+					),
+				};
+
+				return logch.send({ embed: logEmbed });
+			}
+		}
+	},
 };
