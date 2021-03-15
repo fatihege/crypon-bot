89ec939429976ddce54729a62cd042ca6b082e74
Fatih EGE <54741040+fatihege@users.noreply.github.com>
2021-03-15 16:15:28 +0300

    Update invites.js

--- commands/invites.js
+++ commands/invites.js
@@ -1,47 +1,46 @@
+const translate = require('../language/translate');
+
 module.exports = {
-    name: "invites",
-    description: "Bir üyenin kaç kişiyi davet ettiğini görün.",
-    aliases: ["invs", "davetler"],
-    args: false,
-    usage: "<üye[opsiyonel]>",
-    guildOnly: true,
-    permissions: null,
-    run(message, args, client) {
-        let userid;
-        if (message.mentions.users.first()) {
-            userid = message.mentions.users.first().id;
-        } else if (args[0]) {
-            userid = args[0];
-        } else {
-            userid = message.author.id;
-        }
+	name: 'invites',
+	description: null,
+	aliases: ['invs', 'davetler'],
+	args: false,
+	usage: null,
+	guildOnly: true,
+	permissions: null,
+	run(message, args, client) {
+		this.description = translate(message, "commands.invites.description");
+		this.usage = translate(message, "commands.invites.usage");
 
-        const user = message.guild.members.cache.find(
-            (member) => member.id == userid
-        );
+		let userid;
+		if (message.mentions.users.first()) {
+			userid = message.mentions.users.first().id;
+		} else if (args[0]) {
+			userid = args[0];
+		} else {
+			userid = message.author.id;
+		}
 
-        if (!user) {
-            return message.channel
-                .send(`Hey <@${message.author.id}>, böyle bir kullanıcı yok!`)
-                .then((msg) => {
-                    msg.delete({ timeout: 5000 });
-                });
-        }
+		const user = message.guild.members.cache.find((member) => member.id == userid);
 
-        message.guild.fetchInvites().then((invites) => {
-            const userInvites = invites
-                .array()
-                .filter((inv) => inv.inviter.id == userid);
-            var userInvitesCount = 0;
-            userInvites.forEach(
-                (invite) => (userInvitesCount += parseInt(invite.uses))
-            );
-            const embed = {
-                color: 0xff14b9,
-                title: `${user.user.username}#${user.user.discriminator} Kullanıcısının Davetleri`,
-                description: `<@${userid}> kullanıcısının toplam **${userInvitesCount}** daveti var.`
-            };
-            message.channel.send({ embed: embed });
-        });
-    }
+		if (!user) {
+			return message.channel
+				.send(translate(message, 'commands.invites.messages.memberNotExists', message.author.id))
+				.then((msg) => {
+					msg.delete({ timeout: 5000 });
+				});
+		}
+
+		message.guild.fetchInvites().then((invites) => {
+			const userInvites = invites.array().filter((inv) => inv.inviter.id == userid);
+			var userInvitesCount = 0;
+			userInvites.forEach((invite) => (userInvitesCount += parseInt(invite.uses)));
+			const embed = {
+				color: 0xff14b9,
+				title: translate(message, "commands.invites.messages.embedTitle", user.user.tag),
+				description: translate(message, "commands.invites.messages.embedDescription", userid, userInvitesCount),
+			};
+			message.channel.send({ embed: embed });
+		});
+	},
 };
