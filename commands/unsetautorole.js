5991234e2c8ef555cf564e5a06cb740dc2ba69e4
Fatih EGE <54741040+fatihege@users.noreply.github.com>
2021-03-15 16:19:41 +0300

    Update unsetautorole.js

--- commands/unsetautorole.js
+++ commands/unsetautorole.js
@@ -1,35 +1,40 @@
-const db = require("wio.db");
+const db = require('wio.db');
+const translate = require('../language/translate');
 
 module.exports = {
-    name: "unsetautorole",
-    description:
-        "Daha önceden seçilmiş olan otomatik rol etkinliğini kaldırın.",
-    aliases: ["unsetarole", "orolsil", "otorolsil"],
-    args: false,
-    usage: null,
-    guildOnly: true,
-    permissions: "MANAGE_ROLES",
-    async run(message, args, client) {
-        if (await db.fetch("autorole_" + message.guild.id)) {
-            try {
-                await db.delete("autorole_" + message.guild.id);
-                return message.channel
-                    .send(`Otomatik rol etkinliği başarıyla kaldırıldı.`)
-                    .then((msg) => {
-                        msg.delete({ timeout: 5000 });
-                    });
-            } catch (error) {
-                console.error(error);
-                return message.channel.send(
-                    "Otomatik rol kaldırılırken bir hatayla karşılaşıyorum."
-                );
-            }
-        } else {
-            return message.channel
-                .send("Otomatik rol zaten daha önceden ayarlanmamış.")
-                .then((msg) => {
-                    msg.delete({ timeout: 5000 });
-                });
-        }
-    }
+	name: 'unsetautorole',
+	description: null,
+	aliases: ['unsetarole', 'orolsil', 'otorolsil'],
+	args: false,
+	usage: null,
+	guildOnly: true,
+	permissions: 'MANAGE_ROLES',
+	async run(message, args, client) {
+		this.description = translate(message, 'commands.unsetautorole.description');
+		this.usage = translate(message, 'commands.unsetautorole.usage');
+
+		if (await db.fetch('autorole_' + message.guild.id)) {
+			try {
+				await db.delete('autorole_' + message.guild.id);
+				return message.channel
+					.send(translate(message, 'commands.unsetautorole.messages.successful'))
+					.then((msg) => {
+						msg.delete({ timeout: 5000 });
+					});
+			} catch (error) {
+				console.error(error);
+				return message.channel
+					.send(translate(message, 'commands.unsetautorole.messages.errorOccurred'))
+					.then((msg) => {
+						msg.delete({ timeout: 5000 });
+					});
+			}
+		} else {
+			return message.channel
+				.send(translate(message, 'commands.unsetautorole.messages.channelNotSelected'))
+				.then((msg) => {
+					msg.delete({ timeout: 5000 });
+				});
+		}
+	},
 };
