8cb651845f1a826ee06b7566075325538650d8ef
Fatih EGE <54741040+fatihege@users.noreply.github.com>
2021-03-15 16:27:32 +0300

    Update messageDeleteBulk.js

--- events/messageDeleteBulk.js
+++ events/messageDeleteBulk.js
@@ -1,44 +1,40 @@
-const db = require("wio.db");
+const db = require('wio.db');
+const translate = require('../language/translate');
 
 module.exports = {
-    name: "messageDeleteBulk",
-    once: false,
-    async run(messages, client) {
-        if (messages.first().guild) {
-            const message = messages.first();
-            if (await db.fetch("logch_" + message.guild.id)) {
-                const logChannel = await db.fetch("logch_" + message.guild.id);
-                const logch = message.guild.channels.cache.find(
-                    (ch) => ch.id === logChannel
-                );
+	name: 'messageDeleteBulk',
+	once: false,
+	async run(messages, client) {
+		if (messages.first().guild) {
+			const message = messages.first();
+			if (await db.fetch('logch_' + message.guild.id)) {
+				const logChannel = await db.fetch('logch_' + message.guild.id);
+				const logch = message.guild.channels.cache.find((ch) => ch.id === logChannel);
 
-                let logEmbed = {
-                    color: 0xe60ffa,
-                    title: `${messages.size} Mesaj Silindi - #${message.channel.name}`,
-                    description: ""
-                };
+				let logEmbed = {
+					color: 0xe60ffa,
+					title: translate(
+						messages.first(),
+						'events.messageDeleteBulk.messages.embedTitle',
+						messages.size,
+						message.channel.name
+					),
+					description: '',
+				};
 
-                messages.sort().map((message) => {
-                    if (
-                        message.author.bot ||
-                        message.channel.name == logch.name
-                    ) {
-                        logEmbed.description += "";
-                    } else {
-                        logEmbed.description += `[**${message.author.username}#${message.author.discriminator}**] ${message.content}\n`;
-                    }
-                });
+				messages.sort().map((message) => {
+					logEmbed.description += `[**${message.author.tag}**] ${message.content}\n`;
+				});
 
-                if (logEmbed.description.length > 2000) {
-                    logEmbed.description =
-                        "Mesaj boyutu 2000 karakteri aştığı için görüntülenemiyor.";
-                    return logch.send({ embed: logEmbed });
-                }
+				if (logEmbed.description.length > 2000) {
+					logEmbed.description = translate(messages.first(), 'basic.over2000');
+					return logch.send({ embed: logEmbed });
+				}
 
-                if (logEmbed.description != "") {
-                    return logch.send({ embed: logEmbed });
-                }
-            }
-        }
-    }
+				if (logEmbed.description != '') {
+					return logch.send({ embed: logEmbed });
+				}
+			}
+		}
+	},
 };
