cc9b089f3fa2d6c463b43a1d2433b2766d1f41f3
Fatih EGE <54741040+fatihege@users.noreply.github.com>
2021-03-15 16:14:13 +0300

    Update ban.js

--- commands/ban.js
+++ commands/ban.js
@@ -1,70 +1,64 @@
-const db = require("wio.db");
+const db = require('wio.db');
+const translate = require('../language/translate');
 
 module.exports = {
-    name: "ban",
-    description: "Bir üyeyi sunucudan engelleyin.",
-    aliases: ["mban", "memberban", "uyeengelle", "uengelle"],
-    args: true,
-    usage: "<üye> <sebep[opsiyonel]>",
-    guildOnly: true,
-    permissions: "BAN_MEMBERS",
-    async run(message, args, client) {
-        let userid;
-        if (message.mentions.users.size) {
-            userid = message.mentions.users.first().id;
-        } else {
-            userid = args[0];
-        }
-        const user = message.guild.members.cache.find(
-            (member) => member.id == userid
-        );
-        if (!user) {
-            return message.reply("Böyle bir üye yok!").then((msg) => {
-                msg.delete({ timeout: 5000 });
-            });
-        }
+	name: 'ban',
+	description: null,
+	aliases: ['mban', 'memberban', 'uyeengelle', 'uengelle'],
+	args: true,
+	usage: null,
+	guildOnly: true,
+	permissions: 'BAN_MEMBERS',
+	async run(message, args, client) {
+		this.description = translate(message, 'commands.ban.description');
+		this.usage = translate(message, 'commands.ban.usage');
 
-        if (user.id == message.author.id) {
-            return message
-                .reply("Pişşt! Kendini engelleyemezsin.")
-                .then((msg) => {
-                    msg.delete({ timeout: 5000 });
-                });
-        }
+		let userid;
+		if (message.mentions.users.size) {
+			userid = message.mentions.users.first().id;
+		} else {
+			userid = args[0];
+		}
+		const user = message.guild.members.cache.find((member) => member.id == userid);
+		if (!user) {
+			return message.reply(translate(message, 'commands.ban.messages.memberNotExists')).then((msg) => {
+				msg.delete({ timeout: 5000 });
+			});
+		}
 
-        if (user.id == client.user.id) {
-            return message
-                .reply("Hey sen! Beni bu komutu kullanarak engelleyemezsin.")
-                .then((msg) => {
-                    msg.delete({ timeout: 5000 });
-                });
-        }
+		if (user.id == message.author.id) {
+			return message.reply(translate(message, 'commands.ban.messages.cannotBanYourself')).then((msg) => {
+				msg.delete({ timeout: 5000 });
+			});
+		}
 
-        const prefix = await db.fetch("prefix_" + message.guild.id);
-        const reason = args.slice(1).join(" ") || null;
+		if (user.id == client.user.id) {
+			return message.reply(translate(message, 'commands.ban.messages.cannotBanCrypon')).then((msg) => {
+				msg.delete({ timeout: 5000 });
+			});
+		}
 
-        try {
-            message.guild.members.ban(user, { reason: reason });
-            if (reason) {
-                message.channel
-                    .send(`<@${user.id}> \`${reason}\` nedeniyle engellendi.`)
-                    .then((msg) => {
-                        msg.delete({ timeout: 5000 });
-                    });
-            } else {
-                message.channel
-                    .send(`<@${user.id}> başarıyla engellendi.`)
-                    .then((msg) => {
-                        msg.delete({ timeout: 5000 });
-                    });
-            }
-        } catch (error) {
-            console.error(error);
-            message.channel
-                .send(`<@${user.id}> engellenirken bir hata oluştu!`)
-                .then((msg) => {
-                    msg.delete({ timeout: 5000 });
-                });
-        }
-    }
+		const prefix = await db.fetch('prefix_' + message.guild.id);
+		const reason = args.slice(1).join(' ') || null;
+
+		try {
+			message.guild.members.ban(user, { reason: reason });
+			if (reason) {
+				message.channel
+					.send(translate(message, 'commands.ban.messages.memberBannedWithReason', user.id, reason))
+					.then((msg) => {
+						msg.delete({ timeout: 5000 });
+					});
+			} else {
+				message.channel.send(translate(message, 'commands.ban.messages.memberBanned', user.id)).then((msg) => {
+					msg.delete({ timeout: 5000 });
+				});
+			}
+		} catch (error) {
+			console.error(error);
+			message.channel.send(translate(message, 'commands.ban.messages.errorOccurred')).then((msg) => {
+				msg.delete({ timeout: 5000 });
+			});
+		}
+	},
 };
