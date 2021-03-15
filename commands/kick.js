ceb57df6ae0223e9c7fad52419669e3191798431
Fatih EGE <54741040+fatihege@users.noreply.github.com>
2021-03-15 16:15:54 +0300

    Update kick.js

--- commands/kick.js
+++ commands/kick.js
@@ -1,76 +1,77 @@
-const db = require("wio.db");
+const db = require('wio.db');
+const translate = require('../language/translate');
 
 module.exports = {
-    name: "kick",
-    description: "Bir üyeyi sunucudan atın.",
-    aliases: ["mkick", "uyeat"],
-    args: true,
-    usage: "<üye> <sebep[opsiyonel]>",
-    guildOnly: true,
-    permissions: "KICK_MEMBERS",
-    async run(message, args, client) {
-        let userid;
+	name: 'kick',
+	description: null,
+	aliases: ['mkick', 'uyeat'],
+	args: true,
+	usage: null,
+	guildOnly: true,
+	permissions: 'KICK_MEMBERS',
+	async run(message, args, client) {
+		this.description = translate(message, 'commands.kick.description');
+		this.usage = translate(message, 'commands.kick.usage');
 
-        if (message.mentions.members.size) {
-            userid = message.mentions.members.first().id;
-        } else {
-            userid = args[0];
-        }
+		let userid;
 
-        const user = message.guild.members.cache.find((m) => m.id == userid);
+		if (message.mentions.members.size) {
+			userid = message.mentions.members.first().id;
+		} else {
+			userid = args[0];
+		}
 
-        if (!user) {
-            return message.channel.send("Böyle bir üye yok!").then((msg) => {
-                msg.delete({ timeout: 5000 });
-            });
-        }
+		const user = message.guild.members.cache.find((m) => m.id == userid);
 
-        if (user.id == client.user.id) {
-            return message
-                .reply(`Hey sen! Beni bu komutla **atamazsın!**`)
-                .then((msg) => {
-                    msg.delete({ timeout: 5000 });
-                });
-        }
+		if (!user) {
+			return message.channel.send(translate(message, 'commands.kick.messages.memberNotExists')).then((msg) => {
+				msg.delete({ timeout: 5000 });
+			});
+		}
 
-        if (user.hasPermission("ADMINISTRATOR")) {
-            return message
-                .reply("Yavaş ol, bir yönetici kendi sunucusundan atılamaz!")
-                .then((msg) => {
-                    msg.delete({ timeout: 5000 });
-                });
-        }
+		if (user.id == client.user.id) {
+			return message.reply(translate(message, 'commands.kick.messages.cannotKickCrypon')).then((msg) => {
+				msg.delete({ timeout: 5000 });
+			});
+		}
 
-        const reason = args.slice(1).join(" ") || null;
+		if (user.id == message.author.id) {
+			return message.channel.send(translate(message, "commands.kick.messages.cannotKickYourself"))
+				.then((msg) => {
+					msg.delete({ timeout: 5000 });
+				});
+		}
 
-        try {
-            user.kick(reason);
-            if (reason) {
-                return message.channel
-                    .send(
-                        `<@${user.id}> sunucudan başarıyla **atıldı!**\n**Sebebi:** ${reason}`
-                    )
-                    .then((msg) => {
-                        msg.delete({ timeout: 5000 });
-                    });
-            } else {
-                return message.channel
-                    .send(`<@${user.id}> sunucudan başarıyla **atıldı!**`)
-                    .then((msg) => {
-                        msg.delete({ timeout: 5000 });
-                    });
-            }
-        } catch (error) {
-            if (error) {
-                console.error(error);
-                message.channel
-                    .send(
-                        `<@${user.id}> sunucudan atılırken bir **hata oluştu.**`
-                    )
-                    .then((msg) => {
-                        msg.delete({ timeout: 5000 });
-                    });
-            }
-        }
-    }
+		if (user.hasPermission('ADMINISTRATOR')) {
+			return message.reply(translate(message, 'commands.kick.messages.memberIsAdmin')).then((msg) => {
+				msg.delete({ timeout: 5000 });
+			});
+		}
+
+		const reason = args.slice(1).join(' ') || null;
+
+		try {
+			user.kick(reason);
+			if (reason) {
+				return message.channel
+					.send(translate(message, 'commands.kick.messages.memberIsKickedWithReason', user.id, reason))
+					.then((msg) => {
+						msg.delete({ timeout: 5000 });
+					});
+			} else {
+				return message.channel
+					.send(translate(message, 'commands.kick.messages.memberIsKicked', user.id))
+					.then((msg) => {
+						msg.delete({ timeout: 5000 });
+					});
+			}
+		} catch (error) {
+			if (error) {
+				console.error(error);
+				message.channel.send(translate(message, "commands.kick.messages.errorOccurred", user.id)).then((msg) => {
+					msg.delete({ timeout: 5000 });
+				});
+			}
+		}
+	},
 };
