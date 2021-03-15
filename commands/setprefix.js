cad6369b5916db09783037b2deaa150f74f2c217
Fatih EGE <54741040+fatihege@users.noreply.github.com>
2021-03-15 16:18:25 +0300

    Update setprefix.js

--- commands/setprefix.js
+++ commands/setprefix.js
@@ -1,26 +1,31 @@
-const db = require("wio.db");
+const db = require('wio.db');
+const translate = require('../language/translate');
 
 module.exports = {
-    name: "setprefix",
-    description: "Botun önekini değiştirin.",
-    aliases: ["sprefix", "setpref", "prefixayarla"],
-    args: true,
-    usage: "<önek>",
-    guildOnly: true,
-    permissions: "MANAGE_MESSAGES",
-    async run(message, args, client) {
-        try {
-            await db.set("prefix_" + message.guild.id, args[0]);
-            message.channel
-                .send(`Prefix başarıyla \`${args[0]}\` olarak ayarlandı.`)
+	name: 'setprefix',
+	description: null,
+	aliases: ['sprefix', 'setpref', 'prefixayarla'],
+	args: true,
+	usage: null,
+	guildOnly: true,
+	permissions: 'MANAGE_MESSAGES',
+	async run(message, args, client) {
+		this.description = translate(message, 'commands.setprefix.description');
+		this.usage = translate(message, 'commands.setprefix.usage');
+
+		try {
+			await db.set('prefix_' + message.guild.id, args.join(' '));
+			message.channel
+				.send(translate(message, 'commands.setprefix.messages.successful', args.join(' ')))
+				.then((msg) => {
+					msg.delete({ timeout: 5000 });
+				});
+		} catch (error) {
+			console.error(error);
+			return message.channel.send(translate(message, 'commands.setprefix.messages.errorOccurred'))
                 .then((msg) => {
                     msg.delete({ timeout: 5000 });
                 });
-        } catch (error) {
-            console.error(error);
-            message.channel.send(
-                "Prefix değiştirilirken bir hatayla karşılaşıyorum."
-            );
-        }
-    }
+		}
+	},
 };
