549f4cd7b9c17e192acf6ee98e77c8d225cc71aa
Fatih EGE <54741040+fatihege@users.noreply.github.com>
2021-03-15 16:25:25 +0300

    Update guildIntegrationsUpdate.js

--- events/guildIntegrationsUpdate.js
+++ events/guildIntegrationsUpdate.js
@@ -1,22 +1,23 @@
-const db = require("wio.db");
+const db = require('wio.db');
+const translate = require('../language/translate');
 
 module.exports = {
-    name: "guildIntegrationsUpdate",
-    once: false,
-    async run(guild, client) {
-        if (await db.fetch("logch_" + guild.id)) {
-            const logChannel = await db.fetch("logch_" + guild.id);
-            const logch = guild.channels.cache.find(
-                (ch) => ch.id === logChannel
-            );
+	name: 'guildIntegrationsUpdate',
+	once: false,
+	async run(guild, client) {
+		if (await db.fetch('logch_' + guild.id)) {
+			const logChannel = await db.fetch('logch_' + guild.id);
+			const logch = guild.channels.cache.find((ch) => ch.id === logChannel);
 
-            let logEmbed = {
-                color: 0xe60ffa,
-                title: `Entegrasyon Güncellendi`,
-                description: `Sunucunun entegrasyonları güncellendi.`
-            };
+			const aChannel = guild.channels.cache.first();
 
-            return logch.send({ embed: logEmbed });
-        }
-    }
+			let logEmbed = {
+				color: 0xe60ffa,
+				title: translate(aChannel, 'events.guildIntegrationsUpdate.messages.embedTitle'),
+				description: translate(aChannel, 'events.guildIntegrationsUpdate.messages.embedDescription'),
+			};
+
+			return logch.send({ embed: logEmbed });
+		}
+	},
 };
