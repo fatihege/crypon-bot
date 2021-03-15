79e1edc3c404a8427a4e31a034abaefff54314d2
Fatih EGE <54741040+fatihege@users.noreply.github.com>
2021-03-15 16:19:22 +0300

    Update unlogchannel.js

--- commands/unlogchannel.js
+++ commands/unlogchannel.js
@@ -1,35 +1,35 @@
-const db = require("wio.db");
+const db = require('wio.db');
+const translate = require('../language/translate');
 
 module.exports = {
-    name: "unlogchannel",
-    description: "Daha önceden seçilen günlük kaydı kanalını silin.",
-    aliases: ["unlogch", "gunlukkanalisil"],
-    args: false,
-    usage: null,
-    guildOnly: true,
-    permissions: "MANAGE_CHANNEL",
-    async run(message, args, client) {
-        if (message.guild && (await db.fetch("logch_" + message.guild.id))) {
-            try {
-                await db.delete("logch_" + message.guild.id);
-                message.channel
-                    .send("Günlük kaydı kanalı başarıyla silindi!")
-                    .then((msg) => {
-                        msg.delete({ timeout: 5000 });
-                    });
-            } catch (err) {
-                message.channel
-                    .send("Bir hata ile karşılaşıldı.")
-                    .then((msg) => {
-                        msg.delete({ timeout: 5000 });
-                    });
-            }
-        } else {
-            message.channel
-                .send("Günlük kaydı kanalı zaten daha önceden seçilmemiş.")
-                .then((msg) => {
-                    msg.delete({ timeout: 5000 });
-                });
-        }
-    }
+	name: 'unlogchannel',
+	description: null,
+	aliases: ['unlogch', 'gunlukkanalisil'],
+	args: false,
+	usage: null,
+	guildOnly: true,
+	permissions: 'MANAGE_CHANNEL',
+	async run(message, args, client) {
+		this.description = translate(message, 'commands.unlogchannel.description');
+		this.usage = translate(message, 'commands.unlogchannel.usage');
+
+		if (message.guild && (await db.fetch('logch_' + message.guild.id))) {
+			try {
+				await db.delete('logch_' + message.guild.id);
+				message.channel.send(translate(message, 'commands.unlogchannel.messages.successful')).then((msg) => {
+					msg.delete({ timeout: 5000 });
+				});
+			} catch (err) {
+				message.channel.send(translate(message, 'commands.unlogchannel.messages.errorOccurred')).then((msg) => {
+					msg.delete({ timeout: 5000 });
+				});
+			}
+		} else {
+			message.channel
+				.send(translate(message, 'commands.unlogchannel.messages.channelNotSelected'))
+				.then((msg) => {
+					msg.delete({ timeout: 5000 });
+				});
+		}
+	},
 };
