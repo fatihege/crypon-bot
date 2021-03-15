66929dd6560d70e2836dc96d061c884c35d2a6ab
Fatih EGE <54741040+fatihege@users.noreply.github.com>
2021-03-15 16:23:22 +0300

    Update channelUpdate.js

--- events/channelUpdate.js
+++ events/channelUpdate.js
@@ -1,43 +1,52 @@
-const db = require("wio.db");
+const db = require('wio.db');
+const translate = require('../language/translate');
 
 module.exports = {
-    name: "channelUpdate",
-    once: false,
-    async run(oldChannel, newChannel, client) {
-        if (oldChannel.guild) {
-            if (await db.fetch("logch_" + oldChannel.guild.id)) {
-                const logChannel = await db.fetch(
-                    "logch_" + oldChannel.guild.id
-                );
-                const logch = oldChannel.guild.channels.cache.find(
-                    (ch) => ch.id === logChannel
-                );
+	name: 'channelUpdate',
+	once: false,
+	async run(oldChannel, newChannel, client) {
+		if (oldChannel.guild) {
+			if (await db.fetch('logch_' + oldChannel.guild.id)) {
+				const logChannel = await db.fetch('logch_' + oldChannel.guild.id);
+				const logch = oldChannel.guild.channels.cache.find((ch) => ch.id === logChannel);
 
-                let logEmbed;
+				let logEmbed;
 
-                if (oldChannel.type == "category") {
-                    logEmbed = {
-                        color: 0xe60ffa,
-                        title: "Kategori Güncellendi",
-                        description: ""
-                    };
-                } else {
-                    logEmbed = {
-                        color: 0xe60ffa,
-                        title: "Kanal Güncellendi",
-                        description: ""
-                    };
-                }
+				if (oldChannel.type == 'category') {
+					logEmbed = {
+						color: 0xe60ffa,
+						title: translate(oldChannel, 'events.channelUpdate.messages.categoryUpdatedTitle'),
+						description: '',
+					};
+				} else {
+					logEmbed = {
+						color: 0xe60ffa,
+						title: translate(oldChannel, 'events.channelUpdate.messages.channelUpdatedTitle'),
+						description: '',
+					};
+				}
 
-                if (oldChannel.name.toString() != newChannel.name.toString()) {
-                    if (oldChannel.type == "category") {
-                        logEmbed.description = `**${newChannel.name}** Kategorisi güncellendi.\n\n**Eski Ad:** ${oldChannel.name}\n**Yeni Ad:** ${newChannel.name}`;
-                    } else {
-                        logEmbed.description = `<#${newChannel.id}> kanalı güncellendi.\n\n**Eski Ad:** ${oldChannel.name}\n**Yeni Ad:** ${newChannel.name}`;
-                    }
-                    return logch.send({ embed: logEmbed });
-                }
-            }
-        }
-    }
+				if (oldChannel.name.toString() != newChannel.name.toString()) {
+					if (oldChannel.type == 'category') {
+						logEmbed.description = translate(
+							oldChannel,
+							'events.channelUpdate.messages.categoryUpdatedDescription',
+							newChannel.name,
+							oldChannel.name,
+							newChannel.name
+						);
+					} else {
+						logEmbed.description = translate(
+							oldChannel,
+							'events.channelUpdate.messages.channelUpdatedDescription',
+							newChannel.id,
+							oldChannel.name,
+							newChannel.name
+						);
+					}
+					return logch.send({ embed: logEmbed });
+				}
+			}
+		}
+	},
 };
