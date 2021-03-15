72b3bd85f9fc2f35c7fa5f2c6b6f2be0b867f573
Fatih EGE <54741040+fatihege@users.noreply.github.com>
2021-03-15 16:28:37 +0300

    Update roleCreate.js

--- events/roleCreate.js
+++ events/roleCreate.js
@@ -1,28 +1,41 @@
-const db = require("wio.db");
+const db = require('wio.db');
+const translate = require('../language/translate');
 
 module.exports = {
-    name: "roleCreate",
-    once: false,
-    async run(role) {
-        if (role.guild) {
-            if (await db.fetch("logch_" + role.guild.id)) {
-                const logChannel = await db.fetch("logch_" + role.guild.id);
-                const logch = role.guild.channels.cache.find(
-                    (ch) => ch.id === logChannel
-                );
+	name: 'roleCreate',
+	once: false,
+	async run(role) {
+		if (role.guild) {
+			let yes, no;
+			if (translate(role, 'basic.yes') == 'Var') {
+				yes = 'Evet';
+			} else {
+				yes = translate(role, 'basic.yes');
+			}
+			if (translate(role, 'basic.no') == 'Yok') {
+				no = 'Hayır';
+			} else {
+				no = translate(role, 'basic.no');
+			}
 
-                let logEmbed = {
-                    color: 0xe60ffa,
-                    title: `Rol Oluşturuldu`,
-                    description: `**Ad:** ${role.name}\n**Renk:** ${
-                        role.hexColor
-                    }\n**Bahsedilebilir:** ${
-                        role.mentionable ? "Evet" : "Hayır"
-                    }`
-                };
+			if (await db.fetch('logch_' + role.guild.id)) {
+				const logChannel = await db.fetch('logch_' + role.guild.id);
+				const logch = role.guild.channels.cache.find((ch) => ch.id === logChannel);
 
-                return logch.send({ embed: logEmbed });
-            }
-        }
-    }
+				let logEmbed = {
+					color: 0xe60ffa,
+					title: translate(role, 'events.roleCreate.messages.embedTitle'),
+					description: translate(
+						role,
+						'events.roleCreate.messages.embedDescription',
+						role.name,
+						role.hexColor,
+						role.mentionable ? yes : no
+					),
+				};
+
+				return logch.send({ embed: logEmbed });
+			}
+		}
+	},
 };
