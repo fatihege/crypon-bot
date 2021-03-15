5a88f157d019dd541e4297a7af96067f798f74c5
Fatih EGE <54741040+fatihege@users.noreply.github.com>
2021-03-15 16:24:16 +0300

    Update guildBanAdd.js

--- events/guildBanAdd.js
+++ events/guildBanAdd.js
@@ -1,29 +1,27 @@
-const db = require("wio.db");
+const db = require('wio.db');
+const translate = require('../language/translate');
 
 module.exports = {
-    name: "guildBanAdd",
-    once: false,
-    async run(guild, user, client) {
-        if (await db.fetch("logch_" + guild.id)) {
-            const logChannel = await db.fetch("logch_" + guild.id);
-            const logch = guild.channels.cache.find(
-                (ch) => ch.id === logChannel
-            );
+	name: 'guildBanAdd',
+	once: false,
+	async run(guild, user, client) {
+		if (await db.fetch('logch_' + guild.id)) {
+			const logChannel = await db.fetch('logch_' + guild.id);
+			const logch = guild.channels.cache.find((ch) => ch.id === logChannel);
 
-            let logEmbed = {
-                color: 0xe60ffa,
-                author: {
-                    name: `${user.username}#${user.discriminator}`,
-                    icon_url: user.displayAvatarURL({
-                        format: "png",
-                        dynamic: true
-                    })
-                },
-                title: `Üye Engellendi`,
-                description: `<@${user.id}> sunucudan engellendi.`
-            };
+            const aChannel = guild.channels.cache.first();
 
-            return logch.send({ embed: logEmbed });
-        }
-    }
+			let logEmbed = {
+				color: 0xe60ffa,
+				author: {
+					name: `${user.username}#${user.discriminator}`,
+					icon_url: user.displayAvatarURL({ format: 'png', dynamic: true }),
+				},
+				title: translate(aChannel, 'events.guildBanAdd.messages.embedTitle'),
+				description: translate(aChannel, 'events.guildBanAdd.messages.embedDescription', user.id),
+			};
+
+			return logch.send({ embed: logEmbed });
+		}
+	},
 };
