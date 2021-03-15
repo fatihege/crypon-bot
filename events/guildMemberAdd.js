7d1ae6a901e022b39f129462838fd5fc2b837222
Fatih EGE <54741040+fatihege@users.noreply.github.com>
2021-03-15 16:25:47 +0300

    Update guildMemberAdd.js

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -1,41 +1,39 @@
-const db = require("wio.db");
+const db = require('wio.db');
+const translate = require('../language/translate');
 
 module.exports = {
-    name: "guildMemberAdd",
-    once: false,
-    async run(member, client) {
-        if (member.guild) {
-            const guildId = member.guild.id;
-            if (await db.fetch("autorole_" + guildId)) {
-                const autoRole = await db.fetch("autorole_" + guildId);
-                const role = member.guild.roles.cache.find(
-                    (role) => role.id === autoRole
-                );
-                member.roles.add(role);
+	name: 'guildMemberAdd',
+	once: false,
+	async run(member, client) {
+		if (member.guild) {
+			const guildId = member.guild.id;
 
-                if (await db.fetch("welcomech_" + guildId)) {
-                    const welcomechid = await db.fetch("welcomech_" + guildId);
-                    const welcomech = member.guild.channels.cache.find(
-                        (ch) => ch.id == welcomechid
-                    );
-                    const welcomeMessages = [
-                        `Aramıza hoşgeldin ${member}. Seni burada görmek güzel.`,
-                        `Ooo, ${member} gelmiş. Herkes ayağa kalksın!`,
-                        `Yaşasın! Sonunda sende aramıza katıldın ${member}!`,
-                        `Hoşgeldin ${member}. Az kalsın partiyi kaçırıyordun.`,
-                        `N'aber ${member}. Bu akşam yemek senden.`
-                    ];
+			let language;
+			if (await db.fetch('lang_' + guildId)) {
+				language = await db.fetch('lang_' + guildId);
+			} else {
+				language = 'tr';
+			}
 
-                    if (welcomech) {
-                        const randomMessageIndex = Math.floor(
-                            Math.random() * welcomeMessages.length
-                        );
-                        const randomMessage =
-                            welcomeMessages[randomMessageIndex];
-                        welcomech.send(randomMessage);
-                    }
-                }
-            }
-        }
-    }
+			if (await db.fetch('autorole_' + guildId)) {
+				const autoRole = await db.fetch('autorole_' + guildId);
+				const role = member.guild.roles.cache.find((role) => role.id === autoRole);
+				member.roles.add(role);
+			}
+
+			if (await db.fetch('welcomech_' + guildId)) {
+				const welcomechid = await db.fetch('welcomech_' + guildId);
+				const welcomech = member.guild.channels.cache.find((ch) => ch.id == welcomechid);
+				const welcomeMessages = require(`../languages/${language}.json`).events.guildMemberAdd.messages
+					.welcomeMessages;
+
+				if (welcomech) {
+					const randomMessageIndex = Math.floor(Math.random() * welcomeMessages.length);
+					let randomMessage = welcomeMessages[randomMessageIndex];
+					randomMessage = randomMessage.replace(/{(\d)}/gi, member);
+					welcomech.send(randomMessage);
+				}
+			}
+		}
+	},
 };
