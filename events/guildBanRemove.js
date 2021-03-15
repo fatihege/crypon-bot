08b43f69a11f3886ed8171d7af89f51d9eb06d07
Fatih EGE <54741040+fatihege@users.noreply.github.com>
2021-03-15 16:24:31 +0300

    Update guildBanRemove.js

--- events/guildBanRemove.js
+++ events/guildBanRemove.js
@@ -1,22 +1,23 @@
-const db = require("wio.db");
+const db = require('wio.db');
+const translate = require('../language/translate');
 
 module.exports = {
-    name: "guildBanRemove",
-    once: false,
-    async run(guild, user, client) {
-        if (await db.fetch("logch_" + guild.id)) {
-            const logChannel = await db.fetch("logch_" + guild.id);
-            const logch = guild.channels.cache.find(
-                (ch) => ch.id === logChannel
-            );
+	name: 'guildBanRemove',
+	once: false,
+	async run(guild, user, client) {
+		if (await db.fetch('logch_' + guild.id)) {
+			const logChannel = await db.fetch('logch_' + guild.id);
+			const logch = guild.channels.cache.find((ch) => ch.id === logChannel);
 
-            let logEmbed = {
-                color: 0xe60ffa,
-                title: `Kullanıcı Engeli Kaldırıldı`,
-                description: `<@${user.id}> kullanıcısının engeli kaldırıldı.`
-            };
+			const aChannel = guild.channels.cache.first();
 
-            return logch.send({ embed: logEmbed });
-        }
-    }
+			let logEmbed = {
+				color: 0xe60ffa,
+				title: translate(aChannel, 'events.guildBanRemove.messages.embedTitle'),
+				description: translate(aChannel, 'events.guildBanRemove.messages.embedDescription', user.id),
+			};
+
+			return logch.send({ embed: logEmbed });
+		}
+	},
 };
